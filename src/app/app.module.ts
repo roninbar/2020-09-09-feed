5de19d0a43f3cb663b363e4021edcf8a1685de5a
ron <rinbar70@gmail.com>
2020-09-13 17:36:23 +0300

    Add <highcharts-chart> to <app-feed>.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FeedComponent } from './feed/feed.component';
 import { PostComponent } from './post/post.component';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 @NgModule({
   declarations: [
     AppComponent,
     FeedComponent,
-    PostComponent
+    PostComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HighchartsChartModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-feed',
@@ -11,32 +12,88 @@ export class FeedComponent implements OnInit {
       description: 'Stan',
       imageUrl:
         'http://southparkstudios.mtvnimages.com/shared/characters/kids/stan-marsh.png?height=165',
+      likes: 0,
     },
     {
       description: 'Kyle',
       imageUrl:
         'http://southparkstudios.mtvnimages.com/shared/characters/kids/kyle-broflovski.png?height=165',
+      likes: 0,
     },
     {
       description: 'Cartman',
       imageUrl:
         'http://southparkstudios.mtvnimages.com/shared/characters/kids/eric-cartman.png?height=165',
+      likes: 0,
     },
     {
       description: 'Kenny',
       imageUrl:
         'http://southparkstudios.mtvnimages.com/shared/characters/kids/kenny-mccormick.png?height=165',
+      likes: 0,
     },
   ];
 
+  Highcharts: typeof Highcharts = Highcharts; // required
+  chartConstructor: string = 'chart'; // optional string, defaults to 'chart'
+  chartOptions: Highcharts.Options = {
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie',
+    },
+    title: {
+      text: 'Likes',
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>',
+    },
+    accessibility: {
+      point: {
+        valueSuffix: '%',
+      },
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: false,
+        },
+        showInLegend: true,
+      },
+    },
+    series: [
+      {
+        type: 'pie',
+        name: 'Brands',
+        colorByPoint: true,
+        data: this.getChartSeriesData(),
+      },
+    ],
+  }; // required
+  updateFlag: boolean = false; // optional boolean
+  oneToOneFlag: boolean = true; // optional boolean, defaults to false
+  runOutsideAngularFlag: boolean = false; // optional boolean, defaults to false
+
   searchTerm: string = '';
-  totalLikes: number = 0;
 
   constructor() {}
 
-  onLikesChanged({ description, likes }) {
-    console.info(description, likes);
-    this.totalLikes++;
+  private getChartSeriesData() {
+    const totalLikes = this.posts.reduce((a, { likes: b }) => a + b, 0);
+    return this.posts.map(({ description, likes }) => [
+      description,
+      (likes / totalLikes) * 100,
+    ]);
+  }
+
+  onLikesChanged(index, { likes }) {
+    this.posts[index].likes = likes;
+    const series = <Highcharts.SeriesPieOptions>this.chartOptions.series[0];
+    series.data = this.getChartSeriesData();
+    this.updateFlag = true;
   }
 
   onKeyUpSearch({ target: { value } }) {
